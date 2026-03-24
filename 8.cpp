2eabc27d1b6b39e1dfb75b1a0e21fc398233d331
Babich Kyrylo <Kyrylo.Babich@infiz.khpi.edu.ua>
2026-03-24 17:02:33 +0200

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	srand(time(0));
+	double array[15];
+	double maxneg = -100000;
+	for (int i = 0; i < 15; i++) {
+
+		array[i] = (rand() % 1000 - 500) / 100.0;
+		cout << array[i] << " ";
+		if (array[i] < 0) {
+			if (array[i] > maxneg) {
+				maxneg = array[i];
+			}
+		}
+	}
+	cout << "\nThe largest negative number is: " << maxneg;
+	cout << "\nThe rounded value of the largest negative number is: " << round(maxneg);
+}
+
+
